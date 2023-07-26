9c1e0b980305a2b2dc1fe94efe46dbcd74229aa0
codepreacher <75647192+code-preacher@users.noreply.github.com>
2023-07-26 12:54:56 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+    <!-- important meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Title -->
+    <title>PDF READER</title>
+    
+    <!-- Basic SEO -->
+    <meta name="description" content="PDF READER">
+    <meta name="keywords" content="PDF READER">
+
+</head>
+
+<body>
+  
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                              <a class="media" href="pdf/CMP422.pdf"></a>
+                                        </div>
+
+                                </div>
+                                <!-- /# column -->
+                            </div>
+                            <!-- End PAge Content -->
+                        </div>
+                        <!-- End Container fluid  -->
+                        <!-- footer -->
+
+                                       <!-- FOOTER REGION -->
+        
+
+        <!-- End footer -->
+    </div>
+    <!-- End Page wrapper  -->
+</div>
+<!-- End Wrapper -->
+
+
+                <!--PDF VIEWER JavaScript -->
+                <script src="js/jquery-1.12.4.min.js"></script>
+                <script src="js/jquery.media.js"></script>
+                <script src="js/pdf-active.js"></script>
+                
+
+</body>
+
+</html>
